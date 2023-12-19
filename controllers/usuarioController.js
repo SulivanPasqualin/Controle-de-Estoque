ec77ebeaf4309a94c9323f626541cc52e0099925
Sulivan <sulivanpasqualin@live.com>
2023-12-19 16:24:13 -0300

    Ajustes controladores

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -9,21 +9,28 @@ exports.cadastrarUsuario = async (req, res) => {
   try {
     let novoUsuario;
 
-    // Verifique o tipo de usuário e crie um novo registro com base nisso
+    // Criação do usuário comum
+    const { email, senha, tipo_usuario } = req.body;
+    console.log('Dados recebidos:', email, senha, tipo_usuario);
+    novoUsuario = await Usuario.create({ email, senha, tipo_usuario });
+    console.log('Novo usuário criado:', novoUsuario);
+
+    // Verificação do tipo de usuário e criação do registro específico com base nisso
     if (tipo_usuario === 'pf') {
-      const { nome, cpf, email, senha } = req.body;
-      novoUsuario = await Usuario.create({ email, senha, tipo_usuario})
-      novoUsuario = await Usuario_PF.create({ nome, cpf });
+      const { nome, cpf } = req.body;
+      console.log('Dados PF:', nome, cpf);
+      await Usuario_PF.create({ nome, cpf, UsuarioId: novoUsuario.id }); // Associando ao usuário comum
     } else if (tipo_usuario === 'pj') {
-      const { nomeFantasia, razaoSocial, cnpj, email, senha } = req.body;
-      novoUsuario = await Usuario.create({ email, senha, tipo_usuario})
-      novoUsuario = await Usuario_PJ.create({ nomeFantasia, razaoSocial, cnpj });
+      const { nomeFantasia, razaoSocial, cnpj } = req.body;
+      console.log('Dados PJ:', nomeFantasia, razaoSocial, cnpj);
+      await Usuario_PJ.create({ nomeFantasia, razaoSocial, cnpj, UsuarioId: novoUsuario.id }); // Associando ao usuário comum
     }
 
-    // Envie uma resposta de sucesso
+    // Envio de resposta de sucesso
     return res.status(201).json({ message: 'Usuário cadastrado com sucesso!', novoUsuario });
   } catch (error) {
-    // Em caso de erro, envie uma mensagem de erro
+    // Envio de mensagem de erro
+    console.error('Erro ao cadastrar usuário:', error);
     return res.status(500).json({ error: 'Erro ao cadastrar usuário', details: error.message });
   }
 };
